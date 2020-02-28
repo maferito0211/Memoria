32dacfaf7448da7d71d01e9fa3cd4416281a2272
maferito0211 <61156810+maferito0211@users.noreply.github.com>
2020-02-27 23:19:24 -0500

    Add files via upload

---
+++ Juegopares/src/juegopares/Juegopares.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegopares;
+
+
+public class Juegopares {
+
+    
+    public static void main(String[] args) {
+        FrmJuego FrmJuego=new FrmJuego();
+        FrmJuego.setVisible(true);
+    }
+    
+}
